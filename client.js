887ee38f72c7db11eff175dacb148e6b3a336674
Ella Mae A <ellamae207@gmail.com>
2017-09-30 19:28:03 -1000

    prompts clients for username

--- client.js
+++ client.js
@@ -1,17 +1,39 @@
 
 //jshint esversion:6
-
+const PORT = process.env.PORT || 6969;
 const net = require('net');
-const sin = process.stdin;
+/*const sin = process.stdin;
+var name ;
 
 const client = net.connect({port: 6969}, () => {
+  console.log('Now Connected!');
 
+//  socket.emit('connect', username);
+
+//  client.write('Enter You Username:');
   client.on('data', (data) => {
-    console.log(data.toString());
+    console.log(data.toString()); // this console.log whatever 'data' you're typing.
   });
 
   sin.on('data', (data)=>{
     client.write(data);
+
   });
 });
+*/
 
+
+
+const server = new net.Socket();
+server.connect(PORT, '10.0.1.161', () => {
+  console.log(`Connected to server at ${PORT}`);
+
+/*  server.on('data', (data)=> {
+    console.log(data.toString());
+  });*/
+
+  process.stdin.pipe(server);
+
+  server.pipe(process.stdout);
+
+});
--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 //jshint esversion:6
-
-
+/*
+const io = require("socket.io");
 const net = require('net');
-
-
-
 var HOST = '0.0.0.0';
 var PORT = 6969;
+const sin = process.stdin;
 
 
 var clients = [];
 
-const server = net.createServer( (socket) => {
+const server = net.createServer( (socket) => { // socket is dublex
 
-    // We have a connection - a socketet object is assigned to the connection automatically
+    // We have a connection - a socket object is assigned to the connection automatically
+
     console.log('CONNECTED: ' + socket.remoteAddress +':'+ socket.remotePort);
+
+    sin.on('data', (data)=> {
+        socket.write('Broadcast from Server: ' + data);
+    });
+
+
+
 
     // Add a 'data' event handler to this instance of socketet
     socket.on('data', function(data) {
 
-        console.log('CLIENT' + socket.remoteAddress + ': ' + data);
+        console.log('SERVER BCAST FROM ' + socket.remoteAddress + ': ' + data);
         // Write the data back to the socketet, the client will receive it as data from the server
-        socket.write('You said "' + data + '"');
+        socket.write('You said: ' + data + '"');
 
     });
 
@@ -33,8 +39,60 @@ const server = net.createServer( (socket) => {
 }).listen(PORT, HOST);
 
 console.log('Server listening on ' + HOST +':'+ PORT);
+*/
+
+const net = require('net');
+const PORT = process.env.PORT || 6969;
+const clients = [];
+const broadcast = (sender, message ) => clients
+  .filter(c =>
+    c !== sender)
+  .forEach(c => {
+    c.write(message);
+  });
 
 
 
-//socketet.on('data')
+const server = net.createServer((client)=> {
+
+    console.log("A Client has connected");
+
+
+    client.username = null;
+
+    client.write('Please enter your username: \n');
+
+    client.on('data', (data) => {
+        if(client.username === null){
+            client.username = data.toString();
+            client.write(`Welcome ${client.username}`);
+            clients.push(client);
+            console.log('CLIENTS ARRAY: ' + clients);
+        }else{
+            broadcast(client, data.toString());
+//            console.log(client.username + ":" + data.toString());
+            console.log(`${client.username}: ${data.toString()}`);
+        }
+
+         //   client.write(data);
+          //  console.log(data.toString());
+
+
+    });
+
+    process.stdin.on('data', (data)=> {
+        client.write('Broadcast from Server: ' + data);
+    });
+
+    client.on('close', function() {
+        console.log('Connection closed');
+    });
+
+});
+
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
+
 
